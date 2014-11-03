08c042442d0f23e21f01ee1fef2b3fb700469b10
Nevio <nevio.vesic@gmail.com>
2014-11-03 21:26:10 +0100

    Adding revel start/stop/deploy

--- deployment/shell/deploy.sh
+++ deployment/shell/deploy.sh
@@ -2,9 +2,20 @@
 
 HOME_DIR=`eval echo ~`
 PROJECT_PATH=`eval echo ~/gocode/src/neviovesic/`
+PROJECT_NAME="neviovesic" # app name for ogs etc ...
 
 # Activate whatever that we eventually need to activate in order to run deployment
 source $HOME_DIR/.profile
+
+recursiveKill() { # Recursively kill a process and all subprocesses
+    CPIDS=$(pgrep -P $1);
+    for PID in $CPIDS
+    do
+        recursiveKill $PID
+    done
+    sleep 3 && kill -9 $1 2>/dev/null & # hard kill after 3 seconds
+    kill $1 2>/dev/null # try soft kill first
+}
 
 echo -e "$(tput setaf 2)Entering $PROJECT_PATH"
 cd $PROJECT_PATH
@@ -24,6 +35,19 @@ cp $PROJECT_PATH/deployment/nginx/nginx.conf /etc/nginx/
 cp $PROJECT_PATH/deployment/nginx/naxsi_core.rules /etc/nginx/
 cp -r $PROJECT_PATH/deployment/nginx/conf.d/ /etc/nginx/
 
+echo -e "$(tput setaf 2)Stopping revel application (if started) ..."
 
+if [ -f "$PROJECT_PATH/$PROJECT_NAME.pid" ]
+then
+	PID=`cat "$PROJECT_PATH/$PROJECT_NAME.pid"`
+	recursiveKill $PID
+	rm -f "$PROJECT_PATH/$PROJECT_NAME.pid"
+fi
 
+echo -e "$(tput setaf 2)Starting revel applilcation back up ..."
+revel run neviovesic prod > /root/gocode/src/neviovesic/neviovesic.log 2>&1 &
+PID=$!
+echo $PID > "$PROJECT_PATH/$PROJECT_NAME.pid"
+
+echo -e "$(tput setaf 2)Restarting nginx server ..."
 service nginx restart
